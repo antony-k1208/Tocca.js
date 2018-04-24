694979117647c6f116e0e394638eb4aef4a792e9
Antony <ak@digital-essence.de>
2018-04-24 07:39:27 +0200

    Revert "removed XOR event triggering, trigger jQuery and classical event listeners from now on"
    This reverts commit 6f36b251d343a3a4d5e3052589181c51560bc5f6.

--- Tocca.js
+++ Tocca.js
@@ -112,6 +112,12 @@
       data.y = currY
       data.distance = data.distance
 
+      // jquery
+      if (defaults.useJquery) {
+        customEvent = jQuery.Event(eventName, {originalEvent: originalEvent})
+        jQuery(elm).trigger(customEvent, data)
+      }
+
       // addEventListener
       if (customEvent.initEvent) {
         for (var key in data) {
@@ -120,11 +126,6 @@
 
         customEvent.initEvent(eventName, true, true)
         elm.dispatchEvent(customEvent)
-      }
-
-      // jquery
-      if (defaults.useJquery) {
-        jQuery(elm).trigger(jQuery.Event(eventName, {originalEvent: originalEvent}), data)
       }
 
       // detect all the inline events
